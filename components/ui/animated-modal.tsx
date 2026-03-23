258cc1caf7bd21e104652543988a1aadee3da2b6
xiaotianqi <itsupport@zx-studio.com>
2026-03-23 09:00:56 -0500

    feat: update components

--- components/ui/animated-modal.tsx
+++ components/ui/animated-modal.tsx
@@ -11,6 +11,7 @@ import React, {
   useState,
 } from "react";
 import { createPortal } from "react-dom";
+import { useOutsideClick } from "@/hooks/use-outside-click";
 
 interface ModalContextType {
   open: boolean;
@@ -86,7 +87,7 @@ export const ModalBody = ({
     }
   }, [open]);
 
-  const modalRef = useRef(null);
+  const modalRef = useRef<HTMLDivElement>(null);
   const { setOpen } = useModal();
   useOutsideClick(modalRef, () => setOpen(false));
 
@@ -218,27 +219,3 @@ const CloseIcon = () => {
   );
 };
 
-// Hook to detect clicks outside of a component.
-// Add it in a separate file, I've added here for simplicity
-export const useOutsideClick = (
-  ref: React.RefObject<HTMLDivElement>,
-  callback: Function
-) => {
-  useEffect(() => {
-    const listener = (event: any) => {
-      // DO NOTHING if the element being clicked is the target element or their children
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
-      callback(event);
-    };
-
-    document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
-
-    return () => {
-      document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
-    };
-  }, [ref, callback]);
-};
